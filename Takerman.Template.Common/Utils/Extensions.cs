f14cee650c936554cb14802fbeff8e2cb23cb7d9
Tanyo Ivanov <tivanov@takerman.net>
2024-10-19 20:53:42 +0300

    services

---
+++ Takerman.Template.Common/Utils/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Takerman.Template.Common.Utils
+{
+    public static class Extensions
+    {
+        public static string Test(this string input)
+        {
+            return input;
+        }
+    }
+}
